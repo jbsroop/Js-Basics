c78286cb33769af6f301163e8bfd894575f84019
Bala Swaroop Jamgala <jbsroop@gmail.com>
2024-06-26 02:22:34 +0530

    Added Functions File

---
+++ functions1.js
@@ -0,0 +1,9 @@
+function addSquares(a,b){
+    const sa = square(a);
+    const sb = square(b);
+    function square(num){
+        return num * num;
+    }
+    return sa + sb;
+}
+console.log(addSquares(10,10));
